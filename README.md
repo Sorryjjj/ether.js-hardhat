44270178e2e25e0e720cae0cf28c6b3ddec745d1
Sorryjjj <1528164110@qq.com>
2021-12-04 00:04:30 +0800

    README

--- README.md
+++ README.md
@@ -13,3 +13,34 @@ npx hardhat node
 node scripts/sample-script.js
 npx hardhat help
 ```
+
+# Ether.js + hardhat
+
+## ethers.js
+
+https://docs.ethers.io/v5/
+
+ethers.js库旨在为以太坊区块链及其生态系统提供一个小而完整的 JavaScript API 库
+
+对比使用 web3.js 代码量更少，接口也更简洁，推荐优先使用 ethers.js
+
+## hardhat
+
+https://hardhat.org/getting-started/
+
+Hardhat是一个编译、部署、测试和调试以太坊应用的开发环境。
+
+它可以帮助开发人员管理和自动化构建智能合约和dApps过程中固有的重复性任务，并围绕这一工作流程轻松引入更多功能。
+
+这意味着hardhat在最核心的地方是编译、运行和测试智能合约
+
+
+
+1. 创建hardhat项目：https://hardhat.org/guides/project-setup.html
+2. 添加typescript支持：https://hardhat.org/guides/typescript.html
+3. 引入typechain：https://www.npmjs.com/package/@typechain/hardhat
+4. 编译合约 ：yarn compile
+   1. 合约信息保存在artifacts目录下
+   2. 生成的类型信息保存在typechain-types目录下
+5. 执行script： yarn test
+
--- scripts/sample.ts
+++ scripts/sample.ts
@@ -1,12 +1,32 @@
 import { ethers } from "hardhat";
 import { utils } from "ethers";
+/** typechain 对合约进行类型提示 */
 import { Cash } from "../typechain-types/Cash";
 
 (async () => {
+  /**
+   * hardhat中的ethers是对ethers.js的补充
+   * signer是以太坊帐户的抽象，可用于对消息和交易进行签名，并将签名的交易发送到以太坊网络以执行状态更改操作
+   * https://docs.ethers.io/v5/api/signer/#signers
+   */
   let [signer] = await ethers.getSigners();
-  const provider = ethers.provider;
+
+  //   const provider = ethers.provider;
+
+  /**
+   * 创建合约工厂
+   * 通过部署合约的 字节码 以及在 abi 中定义的构造函数来创建合约工厂
+   * getContractFactory简化了这一步
+   */
   let cashFactory = await ethers.getContractFactory("Cash");
+
+  /**
+   * 部署合约
+   */
   let cash = (await cashFactory.connect(signer).deploy()) as Cash;
+  /**
+   * 调用合约方法
+   */
   let bal = await cash.balanceOf(signer.address);
   console.log("bal", utils.formatEther(bal));
 })();
